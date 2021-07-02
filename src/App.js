92649e5f7d761b0b067f59cafb929b993d7a3940
Anle101 <46081192+Anle101@users.noreply.github.com>
2021-07-02 18:27:21 -0400

    Improvements
    Logo implemented properly.
    Randomly selects two fighters.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,56 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import { useState, useEffect, useContext } from 'react';
 import './App.css';
 
-import {useSpring,animated, useChain, useSpringRef} from 'react-spring';
+import {useSpring,animated, useChain, useSpringRef, config} from 'react-spring';
 
 import {globalContext} from './context/globalContext';
 import CharacterSelection from './components/CharacterSelection';
 
 function App() {
-  const titleIntro = useSpringRef;
+
+  const Characters = [
+    {name:"Medusa",CombatRange:"Ranged", minions: "Harpies (3) ", health: "16"},
+    {name:"Sinbad",CombatRange:"Melee", minions: "None", health: "16"},
+    {name:"Alice",CombatRange:"Ranged", minions: "Harpies (3) ", health: "16"},
+    {name:"King Author",CombatRange:"Ranged", minions: "Harpies (3) ", health: "16"},
+    {name:"Robin Hood",CombatRange:"Ranged", minions: "Harpies (3) ", health: "16"},
+    {name:"Bigfoot",CombatRange:"Ranged", minions: "Harpies (3) ", health: "16"}
+  ];
+
+  const titleIntro = useSpringRef();
 
   const titletransition = useSpring({
-      to: {opacity:1,y: 40},
-      from: {opacity:0,y: -200},
+    ref:titleIntro,
+    to: {opacity:1,y: 40},
+    from: {opacity:0,y: -200},
 
-      config: {mass: 1, frequency: 1, damping: 1},
+    config: config.stiff,
     
   });
 
-  const springupIntro = useSpringRef;
+  const springupIntro = useSpringRef();
   const springup = useSpring({
+    ref:springupIntro,
     to: {y:0},
-    from: {y:2000},
+    from: {y:1000},
 
-    delay:300,
-
-    config: {mass: 1, frequency: 1, damping: 1},
+ 
+    config: config.stiff,
    
   });
 
- 
+  function SelectCharacter() { 
+    const random = Math.floor(Math.random() * 6); //0-5
+    const random2 = Math.floor(Math.random() * 6); //0-5
+
+    console.log (Characters[random].name);
+    console.log (Characters[random2].name);
+  }
+  useChain([titleIntro, springupIntro], [0,0]);
   return (
     <>
-      <globalContext.Provider value={springup}>
+      <globalContext.Provider value={SelectCharacter}>
         <div className="App">
             
             <animated.img src={logo} style={titletransition} className="App-logo" alt="logo" />
--- src/components/CharacterSelection.js
+++ src/components/CharacterSelection.js
@@ -1,15 +1,17 @@
 import React, {useContext,useState,useEffect} from 'react'
 import { globalContext } from '../context/globalContext';
+import './CharacterSelection.css';
 
 import {useSpring,animated} from 'react-spring';
 
 function CharacterSelection() {
-
+    
     const springup = useContext(globalContext);
+    const SelectCharacter = useContext(globalContext);
     
     return (
         <>
-            <animated.h1 style={springup}>CHOOSE YOUR CHARACTER</animated.h1>
+            <animated.button style={springup} className="start-button" onClick={SelectCharacter}>GIVE ME MY MATCHUP </animated.button>
         </>
     )
 }
